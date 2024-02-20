5c1ebb6fd9cb4bc88ee9b52c494c78076745c874
matthias03 <maciej.kloda98@gmail.com>
2024-02-20 14:57:02 +0100

    Added cors middleware and allowed local host to acquire data

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const { getData } = require("./db.js");
 
 const port = 3001;
 const app = express();
+const corsOptions = {
+    origin: ["127.0.0.1:3000"]
+}
 
+app.use(cors());
 app.get("/api", async ( req, res)  => {
     const data = await getData();
     res.send(data);
