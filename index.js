7dced77a88af8e2b5cc5a61fdd62453cfc0a2aca
matthias03 <maciej.kloda98@gmail.com>
2024-02-23 17:10:51 +0100

    Edited package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { getData } = require("./db.js");
 
-const port = 3001;
+const port = 3001 ;
 const app = express();
 const corsOptions = {
     origin: ["127.0.0.1:3000","https://online-shop.maciejkloda.pl"]
