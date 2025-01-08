91406be8ee29fc20bc8744d6106cf835e0fefe56
matthias03 <maciej.kloda98@gmail.com>
2025-01-08 21:54:52 +0100

    Changed updateUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,35 +19,29 @@ exports.getUser = async (req,res) => {
     }
 }
 
-exports.updateUser =  async (req,res) => {
+exports.updateUser = async (req, res) => {
     const email = req.body.email;
-    const userData = JSON.parse(req.body.userData);
+    const userData = req.body.userData ? JSON.parse(req.body.userData) : {};
 
     try {
+        let fileUrl = undefined;
+
         if (req.file) {
             const { buffer, originalname } = req.file;
             const blobName = `${Date.now()}-${originalname}`;
-            const fileStream = Buffer.from(buffer).toString("utf-8");
-            const { url } = await put(`avatars/${blobName}`, fileStream, { access: 'public', token: process.env.BLOB_READ_WRITE_TOKEN });
 
-            // const { url } = await put(`avatars/${blobName}`, buffer, {
-            //     access: "public",
-            //     token: process.env.BLOB_READ_WRITE_TOKEN
-            // });
+            const { url } = await put(`avatars/${blobName}`, buffer, {
+                access: 'public',
+                token: process.env.BLOB_READ_WRITE_TOKEN
+            });
+            fileUrl = url; // Ustaw URL przesłanego pliku
+        }
 
-            const data = await list({ prefix: 'avatars/' });
+        await updateUser(email, fileUrl, userData);
 
-            const matchingBlob = data.blobs.find(blob => blob.pathname.endsWith(blobName));
-
-            if (matchingBlob) {
-                await updateUser(email, matchingBlob.url, userData);
-            } else {
-                return res.status(500).json({ message: "Failed to update user profile picture URL" });
-            }
-        } else {
-            await updateUser(email,"",userData);
-        } return res.status(200).json({ status: true });
+        return res.status(200).json({ status: true });
     } catch (err) {
-        return res.status(500).json({ status:false ,message: "Failed to update user" });
+        console.error(err);
+        return res.status(500).json({ status: false, message: "Failed to update user" });
     }
-}
+};
