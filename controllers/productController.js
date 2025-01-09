4741061560d26ebb97fe957faf297c543334729e
matthias03 <maciej.kloda98@gmail.com>
2025-01-09 20:06:51 +0100

    Changed returen data

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,7 @@ exports.getHome = (req, res) => {
 exports.getAllProducts = async (req, res) => {
     try {
         const data = await getData();
-        res.status(200).json({ data });
+        res.status(200).json( data );
     } catch (error) {
         res.status(500).json({ status: false, message: "Failed to fetch products" });
     }
@@ -16,7 +16,7 @@ exports.getAllProducts = async (req, res) => {
 exports.getBestsellers = async (req, res) => {
     try {
         const data = await getBestsellers();
-        res.status(200).json({ data });
+        res.status(200).json( data );
     } catch (error) {
         res.status(500).json({ status: false, message: "Failed to fetch bestsellers" });
     }
@@ -25,7 +25,7 @@ exports.getBestsellers = async (req, res) => {
 exports.getProductsByGender = async (req, res) => {
     try {
         const data = await getDataGender(req.body.gender);
-        res.status(200).json({ data });
+        res.status(200).json( data );
     } catch (error) {
         res.status(500).json({ status: false, message: "Failed to fetch products by gender" });
     }
@@ -34,7 +34,7 @@ exports.getProductsByGender = async (req, res) => {
 exports.searchProducts = async (req, res) => {
     try {
         const data = await getDataByName(req.body.name);
-        res.status(200).json({ data });
+        res.status(200).json( data );
     } catch (error) {
         res.status(500).json({ status: false, message: "Failed to search products" });
     }
@@ -44,7 +44,7 @@ exports.getProductById = async (req, res) => {
     try {
         const data = await getDataById(req.params.id);
         if (data) {
-            res.status(200).json({ data });
+            res.status(200).json( data );
         } else {
             res.status(404).json({ status: false, message: "Product not found" });
         }
